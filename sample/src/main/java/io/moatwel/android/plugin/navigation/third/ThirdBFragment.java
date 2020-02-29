4d03ea13a383fc5021ce1af8282dffd75e27e55e
halu5071 <horiiortho5@gmail.com>
2020-02-29 15:39:53 +0900

    Fix a little

--- sample/src/main/java/io/moatwel/android/plugin/navigation/third/ThirdBFragment.java
+++ sample/src/main/java/io/moatwel/android/plugin/navigation/third/ThirdBFragment.java
@@ -2,5 +2,11 @@ package io.moatwel.android.plugin.navigation.third;
 
 import androidx.fragment.app.Fragment;
 
+import io.moatwel.android.plugin.navigation.R;
+
 public class ThirdBFragment extends Fragment {
+
+    public ThirdBFragment() {
+        super(R.layout.fragment_third_b);
+    }
 }
